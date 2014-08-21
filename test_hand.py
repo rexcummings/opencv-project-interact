0fe19eaa8cf406a86fde9b5ef3eb0df2cd0c489a
rexcummings <rexcummings2012@gmail.com>
2014-08-21 09:05:49 -0400

    Update test_hand.py
    Editing needed and code refinement

--- test_hand.py
+++ test_hand.py
@@ -3,28 +3,30 @@
 Hand tracking using depth sensor. Demonstration of the hand module.  
 
 Author: Rex Cummings, Nathan Sprague
-Version: 1.6, August 10, 2014
+Version: 1.6, August 21, 2014
 
 Note: Mouse Integration with hand tracking achieved through
       utilities from PyUserInput, specifically PyMouse.  
 """ 
 #Custom Import Statements
 import hand 
+import kalman 
 
 #Import Statements
-import cv
+import cv2.cv as cv
 import cv2
 import numpy as np  
+import math
 import time
 import mlpy
 from pymouse import PyMouse
-from pymouse import PyMouseEvent
 
 #Global Variable Declarations
 CENTROID = None
+VERTEX = None
 
 def main():
-    global CENTROID
+    global CENTROID, VERTEX
 
     #Variable Declarations  
     i = 0
@@ -40,6 +42,7 @@ def main():
     white = (255, 255, 255)
     red = (0, 0, 255)    
     blue = (255, 0,0)
+    green = (0, 255, 0)
     orange = (0, 128, 255)    
     begin = False
     initial_roi = True
@@ -48,6 +51,9 @@ def main():
 
     #Instantiate mouse object 
     m = PyMouse()
+
+    #Create Kalman Filter 
+    kf = movingKF()
         
     #Depth camera used
     capture_depth = cv2.VideoCapture(cv2.cv.CV_CAP_OPENNI)
@@ -80,7 +86,7 @@ def main():
         small_pcl_img = pcl_img[15:465, 40:640]
         trigger_box = small_depth_img[25:50, 525:550]
 
-        #Trigger Program
+        #Trigger program
         if begin == False: 
             cv2.rectangle(small_color_img, pt1, pt2, orange, thickness=-1)
             cv2.imshow("small_depth_img", 10 * (small_depth_img/(2**16-1)))
@@ -97,27 +103,26 @@ def main():
                 if totalsum > influence:
                     begin = True
         else:
-            #Start main program
+            """ Start Tracking """
             t = time.clock()
-            print "Time: ", t
-            
+            print "Time: ", t            
+
             #Region of Interest (ROI)
+            depth_roi = np.zeros(depth_roi_shape)
+            pcl_roi = np.zeros(pcl_roi_shape)
             if initial_roi == True:
-                depth_roi = np.zeros(depth_roi_shape)
+                dif_x, dif_y = 400, 0
                 depth_roi = small_depth_img[0:220, 400:600]         
-                pcl_roi = np.zeros(pcl_roi_shape)
                 pcl_roi = small_pcl_img[0:220, 400:600]         
-            else:
-                depth_roi = np.zeros(depth_roi_shape)
-                pcl_roi = np.zeros(pcl_roi_shape)
-                depth_roi, pcl_roi = update_roi(small_depth_img, small_pcl_img, depth_roi_shape, pcl_roi_shape)
+            else:                
+                depth_roi, pcl_roi, dif_x, dif_y = update_roi(small_depth_img, small_pcl_img, depth_roi_shape, pcl_roi_shape)
 
             #Establish thresholded image based on a max threshold of 85mm 
             min_c, max_c, min_loc_c, max_loc_c = cv2.minMaxLoc(depth_roi)
             thresh_max = min_c + thresh    
             ret, thresh_img = cv2.threshold(depth_roi, thresh_max, thresh_max, cv2.THRESH_BINARY_INV) 
             thresh8 = np.array(thresh_img, dtype='uint8')
-  
+
             #Find Hand ROI 
             hand_list = hand.find_blobs(thresh8, 550, 20000)
             print "Blobs: ", len(hand_list)
@@ -129,24 +134,42 @@ def main():
                 print "No hand object detected.\n"
                        
             #Extract information from detected hand object
-            while hand_detected == True:
-                """
-                cloud_roi = hd.reconstruct_img(thresh8, pcl_roi)
-                cv2.imshow("cloud roi", 10 * cloud_roi) 
-                cloud = cv2.GaussianBlur(cloud_roi, (5, 5), 0)
-                sur_area_img = offset_x_dist(cloud, 1) * offset_y_dist(cloud, 1)
-                
-                print "Total SA: ", np.sum(sur_area_img) 
-
-                update_sa_img(sur_area_img, depth_roi, thresh8)
-                """
-                if initial_roi == True:
-                    CENTROID = hd.centroid()
+            while hand_detected:
+                if initial_roi:
+                    x_vertex, y_vertex = 550, 100
+                    cx, cy = hd.centroid()
+                    CENTROID = (cx + 400, cy)               
                 else:
-                    CENTROID = hd.centroid()                        
-                hd.contour_outline(color_img, blue) 
-                cv2.circle(color_img, CENTROID, 10, red, thickness=-1) 
-                """
+                    cx, cy = hd.centroid()                 
+                    xf, yf = (cx + dif_x, cy + dif_y)   
+                    CENTROID = xf, yf               
+                x_vertex, y_vertex = hd.draw_ellipse(initial_roi, depth_roi, small_color_img, dif_x, dif_y, x_vertex, y_vertex, [0,0,0])    
+
+                #Set Kalman Filter                
+                if initial_roi == True:
+                    kf.x[0] = x_vertex
+                    kf.x[1] = y_vertex      
+                    print "Initial: ", kf.x
+                   
+                """ Dynamic Kalman Measurements """
+                z = (x_vertex, y_vertex)
+                
+                #Prediction of new points using KF 
+                kf.predict()                    
+                #Corrections
+                kf.correct(z)  
+              
+                coords = (int(kf.x[0]), int(kf.x[1]))
+                vel_coords = (int(kf.x[0]) + int(5*(kf.x[2])), int(kf.x[1]) + int(5*(kf.x[3])))
+                cv2.circle(small_color_img, coords, 5, orange, thickness=-1)
+                cv2.line(small_color_img, coords, vel_coords, green, thickness=2)
+                
+                print "Orange: ", kf.x[0], kf.x[1]
+                print "diff: ", (kf.x[0] - x_vertex, kf.x[1] - y_vertex)
+
+                #move_mouse(kf.x, m, small_color_img) 
+                """    
+                
                 avg_depth = int(hd.mean_depth(thresh_img))  
                 avg_distances = avg_distances[1:]
                 avg_distances = np.append(avg_distances, avg_depth)
@@ -155,27 +178,71 @@ def main():
                     check_distances = avg_distances - min_avg          
                     #Check if distances match standard click sequence   
                     success_click = is_mouse_click(check_distances)
-                    """
+                
                 #Control mouse with hand 
                 pos = m.position()  
-                print 'MOUSE POSITION: ' + repr(pos)
-                """
-                cx, cy = centroid
-                move_mouse(cx, cy, m, capture_depth)               
-                click_mouse(color_img, centroid, success_click, m)
-                """          
+                print "MOUSE POSITION: ", pos
+                              
+                #click_mouse(color_img, centroid, success_click, m)
+                """      
                 initial_roi = False                                    
                 hand_detected = False
             #Display image versions
-            cv2.imshow("small_depth_img", 10 * (small_depth_img / (2**16-1)))
+            #cv2.imshow("small_depth_img", 10 * (small_depth_img / (2**16-1)))
             cv2.imshow("small_color_img", small_color_img)
-            cv2.imshow("small_pcl_img", small_pcl_img)
+            #cv2.imshow("small_pcl_img", small_pcl_img)
             cv2.imshow("thresh_img", thresh_img)
-            cv2.imshow("depth_roi", 10 * (depth_roi / (2**16-1)))
-            cv2.imshow("pcl_roi", pcl_roi)  
+            #cv2.imshow("depth_roi", 10 * (depth_roi / (2**16-1)))
+            #cv2.imshow("pcl_roi", pcl_roi)  
 
         #Wait only 1 ms before repeating loop.
         c = cv2.waitKey(1)
+
+def move_mouse(kf_x, m, img):
+    """
+    move_mouse -- Moves mouse according to centroid of a Hand object 
+                  and based on resolution ratios.    
+
+    Parameters:
+       xm - x-axis position of mouse
+       ym - y-axis position of mouse 
+       m - pymouse object 
+       color_img - image of window to obtain dimensions from
+       depth_roi - ROI window dimensions     
+    """
+    win_bar = 25
+    mx, my = m.position()    
+    x_screen, y_screen = m.screen_size()
+    win_height, win_width, channels = img.shape
+
+    x_ch = kf_x[0] * (x_screen / win_width) 
+    y_ch = kf_x[1] * (y_screen / win_height + win_bar)
+    print "x_ch: ", x_ch
+    print "y_ch: ", y_ch
+    xf = x_ch + kf_x[2]
+    yf = y_ch + kf_x[3]
+    print "Mouse Pos: ", xf, yf
+    m.move(xf, yf) 
+
+def movingKF():
+    """ Build and return a Kalman filter object for tracking
+    a 2-D object moving at a fixed velocity """
+    Phi =  np.array([[1., 0, 1, 0], # State transition matrix.
+                     [0., 1, 0, 1],
+                     [0., 0, 1, 0],
+                     [0., 0, 0, 1]])
+    C_v =  np.array([[.01, 0, 0, 0],  # Covariance of state transition noise.
+                     [0., .01, 0, 0],
+                     [0., 0, .01, 0],
+                     [0., 0, 0, .01]])
+    Lambda = np.array([[1., 0, 0, 0], 
+                       [0., 1, 0, 0]])
+    C_w =  np.array([[.1, 0],  # Covariance of sensor noise.
+                     [0., .1]])
+    x_0 = np.array([.1, .1, 0, 0]) # initial state estimate.
+    P_0 =  np.eye(4) * 1. # initial Covariance estimate. 
+
+    return kalman.KalmanFilter(Phi, C_v, Lambda, C_w, x_0, P_0)
     
 def offset_x_dist(img, offset):
     diffs = np.zeros(img.shape)
@@ -200,41 +267,56 @@ def update_roi(small_depth_img, small_pcl_img, depth_roi_shape, pcl_roi_shape):
     right = 600 - x
     complete = False
     up_flag, down_flag, left_flag, right_flag = False, False, False, False
-    diff_up, diff_down = 110 - up, 110 - down  
-    diff_left, diff_right = 100 - left, 100 - right 
+    diff_up, diff_down = abs(110 - up), abs(110 - down)  
+    diff_left, diff_right = abs(100 - left), abs(100 - right) 
+    
     while complete == False:
+        #Check corners, then sides
         if up <= 110 and up_flag == False:
-            p1_y = 0
-            up_flag = True
-        elif up > 110 and up_flag == False:
-            p1_y = y - (110 + diff_down)
-            up_flag = True
+            p1_y = 0; p2_y = 220
+            if left > 100 and right > 100:
+                p2_x = x - 100; p1_x = x + 100
+                left_flag = True; right_flag = True
+            up_flag = True; down_flag = True
         elif right <= 100 and right_flag == False:
-            p1_x = 600        
-            right_flag = True      
-        elif right > 100 and right_flag == False:         
-            p1_x = x + diff_left + 100
-            right_flag = True        
+            p1_x = 600; p2_x = 400        
+            if up > 110 and down > 110:
+                p1_y = y - 110; p2_y = y + 110
+                up_flag = True; down_flag = True
+            right_flag = True; left_flag = True         
         elif down <= 110 and down_flag == False:
-            p2_y = 450
-            down_flag = True
-        elif down > 110 and down_flag == False:
-            p2_y = y + diff_up + 110
-            down_flag = True
+            p2_y = 450; p1_y = 230
+            if left > 100 and right > 100:
+                p2_x = x - 100; p1_x = x + 100
+                left_flag = True; right_flag = True
+            down_flag = True; up_flag = True
         elif left <= 100 and left_flag == False:
-            p2_x = 0
-            left_flag = True
+            p2_x = 0; p1_x = 200
+            if up > 110 and down > 110:
+                p1_y = y - 110; p2_y = y + 110
+                up_flag = True; down_flag = True
+            left_flag = True; right_flag = True
+        #Check center 
+        elif up > 110 and up_flag == False:
+            p1_y = diff_up
+            up_flag = True              
+        elif right > 100 and right_flag == False:         
+            p1_x = diff_left + 200
+            right_flag = True       
+        elif down > 110 and down_flag == False:
+            p2_y = diff_up + 220
+            down_flag = True       
         elif left > 100 and left_flag == False:
-            p2_x = x - (100 + diff_right)
-            left_flag = True
+            p2_x = diff_left
+            left_flag = True                
         elif up_flag == True and down_flag == True and left_flag == True and right_flag == True:
             complete = True
     #Shift ROI appropriately  
     depth_roi = np.zeros(depth_roi_shape)
     pcl_roi = np.zeros(pcl_roi_shape)
     depth_roi = small_depth_img[p1_y:p2_y, p2_x:p1_x] 
-    pcl_roi = small_pcl_img[p1_y:p2_y, p2_x:p1_x]        
-    return depth_roi, pcl_roi
+    pcl_roi = small_pcl_img[p1_y:p2_y, p2_x:p1_x]      
+    return depth_roi, pcl_roi, p2_x, p1_y
 
 def update_sa_img(sur_area_img, depth_roi, thresh8):
     over_break_pt = False
@@ -281,27 +363,25 @@ def click_mouse(color_img, centroid, success_click, m):
 
     else:
         print "No mouse click registered. \n"    
-    
-def move_mouse(cx, cy, m, capture_depth):
-    """
-    move_mouse -- Moves mouse according to centroid of a Hand object 
-                  and based on resolution ratios.    
 
-    Parameters:
-       xm - x-axis position of mouse
-       ym - y-axis position of mouse 
-       m - pymouse object 
-       capture_depth - depth camera to obtain dimensions from
-    """
-    x_screen, y_screen = m.screen_size()
-        
-    cam_height = capture_depth.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
-    cam_width = capture_depth.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
-    #print "cam h: ", cam_height
-    #print "cam w: ", cam_width
-    xf = cx * (x_screen / cam_height) 
-    yf = cy * (y_screen / cam_width)
-    m.move(xf, yf)
+def mouse_glide_to(x, y):
+    """Smooth glides mouse from current position to point x,y with specific timing and speed"""
+    #x and y are determined by previous centroid position 
+    x1, y1 = m.position
+    o_x, o_y, o_z = pcl_roi[y1, x1] 
+    n_x, n_y, n_z = pcl_roi[y2, x2] 
+    velocity = abs(sqrt((n_x-o_x)**2 + (n_y-o_y)**2 + (n_z-o_z)**2))    
+    smooth_glide_mouse(x1,y1, x, y, velocity)
+ 
+def smooth_glide_mouse(x1,y1,x2,y2, t, velocity):
+    """Smoothly glides mouse from x1,y1, to x2,y2 in time t using intervals amount of intervals"""
+    distance_x = x2-x1
+    distance_y = y2-y1
+    #need to debug and find proper intervals
+    #use velocity to gauge distance traveled    
+    for n in range(0, intervals+1):
+        m.move(x1 + n * (distance_x/intervals), y1 + n * (distance_y/intervals))
+        time.sleep(t*1.0/intervals)    
 
 def is_mouse_click(check_distances):
     """
